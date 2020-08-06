20fcb7dff004e11544c967e522987753ec79df46
Tanesh Gupta <taneshgupta2211@gmail.com>
2020-08-06 19:30:48 +0530

    add PetProvider class and URIs to Contract class

--- app/src/main/java/com/example/pets_app/data/PetContract.java
+++ app/src/main/java/com/example/pets_app/data/PetContract.java
@@ -16,6 +16,7 @@
 package com.example.pets_app.data;
 
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -38,6 +39,10 @@ public final class PetContract {
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
 
+        public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+        public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+        public static final String PATH_PETS = "pets";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
     }
 
